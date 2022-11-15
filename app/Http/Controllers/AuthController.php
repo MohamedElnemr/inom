b92f8cb30890766a0dfdc506651645a467f3fd07
MohamedElnemr <tigerelnemr077@gmail.com>
2022-11-15 19:48:31 +0200

    auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Models\PasswordReset;
+use App\Notifications\ResetPasswordNotification;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name'=>'required|string|between:3,15',
+            'email'=>'required|email|unique:users',
+            'password'=>'required',
+            'phone'=>'required',
+            'photo'=>'required'
+            ]);
+        $user = new User([
+            'name'=> $request->input('name'),
+            'email'=> $request->input('email'),
+            'password'=> bcrypt($request->input('password')),
+            'phone'=>$request->input('phone'),
+            'photo'=>$request->input('photo'),         
+        ]);
+        $user->save();
+        // User::create($request->getAttributes())->sendEmailVericationNotification();
+        return response()->json(['message'=>'Successfully Created user'],201);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth('api')->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,25 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('logout',[App\Http\Controllers\AuthController::class, 'logout'] );
+    Route::post('register',[App\Http\Controllers\AuthController::class, 'register'] );
+    Route::post('refresh',[App\Http\Controllers\AuthController::class, 'refresh'] );
+    Route::post('me',[App\Http\Controllers\AuthController::class, 'me'] );
+
+});
+
+Route::get('/', function () {
+    return 'hello';
+});
+
+
